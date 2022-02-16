fd68153c4f5a78d2d611fc5ac993afce11657647
giesoko <pramudya.soko@gmail.com>
2022-02-16 21:25:49 +0700

    Using requests package

--- latest_earthquake/__init__.py
+++ latest_earthquake/__init__.py
@@ -1,4 +1,3 @@
-
 """
 Date: 13 Februari 2022
 Time: 17:12:16 WIB
@@ -9,23 +8,37 @@ Location:
 Center: 247 km BaratLaut MELONGUANE-SULUT
 Descripition: tidak berpotensi TSUNAMI
 """
+import requests
+import latest_earthquake
+from bs4 import BeautifulSoup
 
 
 def data_extraction():
-    result_data = dict()
-    result_data['date'] = '13 Februari 2022'
-    result_data['time'] = '17:12:16 WIB'
-    result_data['mag'] = 5.0
-    result_data['location'] = {
-        'lat': 5.94, 'long': 125.59
-    }
-    result_data['center'] = '247 km Barat Laut MELONGUANE-SULUT'
-    result_data['desc'] = 'tidak berpotensi Tsunami'
+    try:
+        content = requests.get('https://www.bmkg.go.id')
+    except Exception:
+        return None
+    if content.status_code == 200:
+        print(content.text)
+    #    soup = BeautifulSoup(content)
+        result_data = dict()
+        result_data['date'] = '13 February 2022'
+        result_data['time'] = '17:12:16 WIB'
+        result_data['mag'] = 5.0
+        result_data['location'] = {
+            'lat': 5.94, 'long': 125.59
+        }
+        result_data['center'] = '247 km Barat Laut MELONGUANE-SULUT'
+        result_data['desc'] = 'Tidak berpotensi Tsunami'
 
-    return result_data
-
+        return result_data
+    else:
+        return None
 
 def show_data(result):
+    if result is None:
+        print('Cannot find the data')
+        return
     print('Source: BMKG.co.id')
     print(f'Date: {result["date"]}')
     print(f'Time: {result["time"]}')
@@ -33,3 +46,9 @@ def show_data(result):
     print(f'Location: lat={result["location"]["lat"]} long={result["location"]["long"]}')
     print(f'Center: {result["center"]}')
     print(f'Description: {result["desc"]}')
+
+
+if __name__ == '__main__':
+    print('Latest Earthquake Detection App')
+    result = latest_earthquake.data_extraction()
+    latest_earthquake.show_data(result)
