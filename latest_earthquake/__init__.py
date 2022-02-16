39ffc0a506b08f5dff3f27f0d357e530c14c860b
giesoko <pramudya.soko@gmail.com>
2022-02-16 14:05:50 +0700

    Modularization with package using import

---
+++ latest_earthquake/__init__.py
@@ -0,0 +1,35 @@
+
+"""
+Date: 13 Februari 2022
+Time: 17:12:16 WIB
+Magnitude: 5.0
+Depth: 10 km
+Location:
+    Lat=5.94 LU - Long=125.59 BT
+Center: 247 km BaratLaut MELONGUANE-SULUT
+Descripition: tidak berpotensi TSUNAMI
+"""
+
+
+def data_extraction():
+    result_data = dict()
+    result_data['date'] = '13 Februari 2022'
+    result_data['time'] = '17:12:16 WIB'
+    result_data['mag'] = 5.0
+    result_data['location'] = {
+        'lat': 5.94, 'long': 125.59
+    }
+    result_data['center'] = '247 km Barat Laut MELONGUANE-SULUT'
+    result_data['desc'] = 'tidak berpotensi Tsunami'
+
+    return result_data
+
+
+def show_data(result):
+    print('Source: BMKG.co.id')
+    print(f'Date: {result["date"]}')
+    print(f'Time: {result["time"]}')
+    print(f'Magnitude: {result["mag"]}')
+    print(f'Location: lat={result["location"]["lat"]} long={result["location"]["long"]}')
+    print(f'Center: {result["center"]}')
+    print(f'Description: {result["desc"]}')
--- main.py
+++ main.py
@@ -1,45 +1,15 @@
 """
 Latest Earthquake Detection App
 modularization with function
+modularization with package
 """
+#First, we can use the following syntax to connect main.py with latest_earthquake package:
+#from latest_earthquake import data_extraction, show_data
 
-"""
-Date: 13 Februari 2022
-Time: 17:12:16 WIB
-Magnitude: 5.0
-Depth: 10 km
-Location: 
-    Lat=5.94 LU - Long=125.59 BT
-Center: 247 km BaratLaut MELONGUANE-SULUT
-Descripition: tidak berpotensi TSUNAMI
-"""
-
-
-def data_extraction():
-    result_data = dict()
-    result_data['date'] = '13 Februari 2022'
-    result_data['time'] = '17:12:16 WIB'
-    result_data['mag'] = 5.0
-    result_data['location'] = {
-        'lat': 5.94, 'long': 125.59
-    }
-    result_data['center'] = '247 km Barat Laut MELONGUANE-SULUT'
-    result_data['desc'] = 'tidak berpotensi Tsunami'
-
-    return result_data
-
-
-def show_data(result):
-    print('Source: BMKG.co.id')
-    print(f'Date {result["date"]}')
-    print(f'Time {result["time"]}')
-    print(f'Magnitude {result["mag"]}')
-    print(f'Location: lat={result["location"]["lat"]} long={result["location"]["long"]}')
-    print(f'Center {result["center"]}')
-    print(f'Description {result["desc"]}')
-
+#we can also use import syntax:
+import latest_earthquake
 
 if __name__ == '__main__':
     print('Latest Earthquake Detection App')
-    result = data_extraction()
-    show_data(result)
+    result = latest_earthquake.data_extraction()
+    latest_earthquake.show_data(result)
