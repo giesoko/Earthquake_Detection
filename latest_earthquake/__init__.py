c416dfabc211cfbb5f92f84fed5f7a697172398f
giesoko <pramudya.soko@gmail.com>
2022-02-16 22:58:31 +0700

    scrape data using BeautifulSoup package

--- latest_earthquake/__init__.py
+++ latest_earthquake/__init__.py
@@ -19,17 +19,54 @@ def data_extraction():
     except Exception:
         return None
     if content.status_code == 200:
-        print(content.text)
-    #    soup = BeautifulSoup(content)
+        soup = BeautifulSoup(content.text, 'html.parser')
+
+        title = soup.find('title')
+        print(title.string)
+
+        time = soup.find('span', {'class': 'waktu'})
+        date = time.text.split(', ')[0]
+        time = time.text.split(', ')[1]
+
+        result = soup.find('div', {'class': 'col-md-6 col-xs-6 gempabumi-detail no-padding'})
+        result = result.findChildren('li')
+        n = 0
+        magnitude = None
+        depth = None
+        lat = None
+        long = None
+        center = None
+        desc = None
+
+        for i in result:
+            #print(n, i)
+            if n == 1:
+                magnitude = i.text
+            elif n ==2:
+                depth = i.text
+            elif n ==3:
+                coordinate = i.text.split(' - ')
+                lat = coordinate[0]
+                long = coordinate[1]
+            elif n == 4:
+                center = i.text
+            elif n == 5:
+                desc = i.text
+            n = n+1
+
+
+
+
         result_data = dict()
-        result_data['date'] = '13 February 2022'
-        result_data['time'] = '17:12:16 WIB'
-        result_data['mag'] = 5.0
-        result_data['location'] = {
-            'lat': 5.94, 'long': 125.59
+        result_data['date'] = date
+        result_data['time'] = time
+        result_data['mag'] = magnitude
+        result_data['depth'] = depth
+        result_data['coordinate'] = {
+            'lat': lat, 'long': long
         }
-        result_data['center'] = '247 km Barat Laut MELONGUANE-SULUT'
-        result_data['desc'] = 'Tidak berpotensi Tsunami'
+        result_data['center'] = center
+        result_data['desc'] = desc
 
         return result_data
     else:
@@ -39,11 +76,12 @@ def show_data(result):
     if result is None:
         print('Cannot find the data')
         return
-    print('Source: BMKG.co.id')
+    print('Source: https://www.BMKG.co.id')
     print(f'Date: {result["date"]}')
     print(f'Time: {result["time"]}')
     print(f'Magnitude: {result["mag"]}')
-    print(f'Location: lat={result["location"]["lat"]} long={result["location"]["long"]}')
+    print(f'Depth: {result["depth"]}')
+    print(f'Coordinate: lat={result["coordinate"]["lat"]} long={result["coordinate"]["long"]}')
     print(f'Center: {result["center"]}')
     print(f'Description: {result["desc"]}')
 
